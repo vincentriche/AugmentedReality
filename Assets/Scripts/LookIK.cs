68045bc8e16c0d4456acd002214788f3a4dfc96a
Vincent <richeounet@gmail.com>
2018-01-25 09:45:17 +0100

    Look Inverse Kinematic

---
+++ Assets/Scripts/LookIK.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Animator))]
+public class LookIK : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject lookObject;
+
+	private Animator animator;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+    void OnAnimatorIK(int layerIndex)
+	{
+		if(lookObject != null)
+		{
+			animator.SetLookAtWeight(1);
+			animator.SetLookAtPosition(lookObject.transform.position);
+		}
+    }
+}
